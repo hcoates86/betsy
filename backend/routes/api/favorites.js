97d8053acc1cc4d5b87409f3afb4516f04957597
Helen Coates <h-coates@live.com>
2023-11-09 23:50:50 -0500

    styling

---
+++ backend/routes/api/favorites.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { requireAuth } = require('../../utils/auth');
+
+const { Category, ProductListing, User } = require('../../db/models');
+
+const router = express.Router();
+
+//view user's favorites
+router.get('/:userId', async (req, res, next) => {
+    
+})
+
+//add to favorites
+router.post('/:productId', async (req, res, next) => {
+    
+})
+
+
+
+module.exports = router;
